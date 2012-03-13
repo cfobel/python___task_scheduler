ace8f6c1979f804474d9cdd8f42688b210220f89
Christian Fobel <christian@fobel.net>
2012-03-13 14:19:00 -0400

    Add TaskScheduler class
    Add TaskScheduler class, which schedules a set of tasks in order, based on
    specified ordering constraints.
    The TaskScheduler currently utilizes the python-constraint library available
    here:
            https://github.com/cfobel/python___labix_constraint
    However, the API exposed by the TaskScheduler class allows for any constraint
    solver to be used as a backend.

--- __init__.py
+++ __init__.py
@@ -5,20 +5,61 @@ less_than = lambda x, y: x < y
 greater_than = lambda x, y: x > y
 
 
+LessThanConstraint = FunctionConstraint(less_than)
+GreaterThanConstraint = FunctionConstraint(greater_than)
+
+
+class TaskScheduler(object):
+    def __init__(self, task_names):
+        self.task_names = task_names[:]
+        self.problem = Problem()
+        self.reset()
+
+    def reset(self):
+        self.problem.reset()
+        self.problem.addVariables(self.task_names, range(len(self.task_names)))
+        # Require that only one task is scheduled for each step
+        self.problem.addConstraint(AllDifferentConstraint())
+
+    def request_order(self, before, after):
+        assert(before in self.task_names)
+        assert(after in self.task_names)
+        self.problem.addConstraint(LessThanConstraint, [before, after])
+
+    def get_schedule(self):
+        solutions = sorted(sorted(x.items())
+                        for x in self.problem.getSolutions())
+        if solutions:
+            return [task for task, position in sorted(list(solutions[0]),
+                    key=lambda x: x[1])]
+        return []
+
+
 if __name__ == '__main__':
-    # Dependency scheduler
-    problem = Problem()
     tasks = list('ABCD')
 
-    problem.addVariables(tasks, range(len(tasks)))
-    problem.addConstraint(AllDifferentConstraint())
+    scheduler = TaskScheduler(tasks)
 
     # Schedule B before A
-    problem.addConstraint(FunctionConstraint(less_than), ['B', 'A'])
-    problem.addConstraint(FunctionConstraint(greater_than), ['A', 'B'])
+    scheduler.request_order('B', 'A')
+    # Schedule D before A
+    scheduler.request_order('D', 'A')
 
-    solutions = sorted(sorted(x.items()) for x in problem.getSolutions())
-    if not solutions:
+    schedule = scheduler.get_schedule()
+    if not schedule:
         print 'Could not meet constraints'
     else:
-        print [task for task, position in sorted(list(solutions[0]), key=lambda x: x[1])]
+        print schedule
+
+    scheduler.reset()
+
+    # Schedule B before A
+    scheduler.request_order('B', 'A')
+    # Schedule A before B
+    scheduler.request_order('A', 'B')
+
+    schedule = scheduler.get_schedule()
+    if not schedule:
+        print 'Could not meet constraints'
+    else:
+        print schedule
